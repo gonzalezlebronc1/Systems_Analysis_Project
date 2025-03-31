a5cc1b1e7094365651f5df0b81e69b63e2a8727a
NepGamer <phuongp1@udayton.edu>
2025-03-31 12:23:04 -0400

    Added major code to application code

--- code/application code/student.java
+++ code/application code/student.java
@@ -1,20 +1,51 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
 //Represents a student applying to the university
+
 class Student {
- private String name;
- private int age;
- private String email;
 
- public Student(String name, int age, String email) {
-     this.name = name;
-     this.age = age;
-     this.email = email;
- }
+    private String name;
 
- public String getName() { return name; }
- public int getAge() { return age; }
- public String getEmail() { return email; }
+    private int age;
+
+    private String email;
+
+    private String major;
+
+
+    public Student(String name, int age, String email, String major) {
+
+        this.name = name;
+
+        this.age = age;
+
+        this.email = email;
+
+        this.major = major;
+
+    }
+
+
+    public String getName() { return name; }
+
+    public int getAge() { return age; }
+
+    public String getEmail() { return email; }
+
+    public String getMajor() { return major; }
+
+
+    public void requestMajorChange(String newMajor, UniversitySystem system) {
+
+        system.processMajorChange(this, newMajor);
+
+    }
+
+
+    public void updateMajor(String newMajor) {
+
+        this.major = newMajor;
+
+        System.out.println(name + " has successfully changed their major to " + newMajor);
+
+    }
+
 }
